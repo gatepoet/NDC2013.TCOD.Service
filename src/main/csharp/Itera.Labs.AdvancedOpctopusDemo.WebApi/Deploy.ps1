a19c56d9e3721ff38a2a213924e0f40dad87a0b0
gatepoet <kristoffer.deinoff@gmail.com>
2013-02-06 02:06:42 +0100

    Fixed package reference

--- src/main/csharp/Itera.Labs.AdvancedOpctopusDemo.WebApi/Deploy.ps1
+++ src/main/csharp/Itera.Labs.AdvancedOpctopusDemo.WebApi/Deploy.ps1
@@ -5,12 +5,14 @@
 
 function Install-WebApi-Service
 {
-    Write-Host "Create the ASPNETDB SQL Server database for the membership system"
+    Write-Host "Installing service"
     $frameworkDir = Get-FrameworkDirectory
     Set-Alias install_util (Join-Path $frameworkDir "installutil.exe")
     install_util Itera.Labs.AdvancedOpctopusDemo.WebApi.exe | Write-Host   
 
-	netsh http add urlacl url=http://+:$Env:Port/ user="NT AUTHORITY\NETWORK SERVICE"
+    Write-Host "Setting rights"
+	netsh http add urlacl url=http://+:$Env:Port/ user="NT AUTHORITY\NETWORK SERVICE" | Write-Host 
 }
 Install-WebApi-Service
+Write-Host "Starting service"
 Start-Service Itera.Labs.AdvancedOpctopusDemo.WebApi | Write-Host
