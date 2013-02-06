0a82d7d1dd10c5b9d96f59568fe8e72b4b32047d
gatepoet <kristoffer.deinoff@gmail.com>
2013-02-06 02:35:56 +0100

    fixed script(hopefully)

--- src/main/csharp/Itera.Labs.AdvancedOpctopusDemo.WebApi/Deploy.ps1
+++ src/main/csharp/Itera.Labs.AdvancedOpctopusDemo.WebApi/Deploy.ps1
@@ -10,7 +10,7 @@ function Install-WebApi-Service
     Set-Alias install_util (Join-Path $frameworkDir "installutil.exe")
     install_util Itera.Labs.AdvancedOpctopusDemo.WebApi.exe | Write-Host   
 
-    Write-Host "Setting rights on http://+:$OctopusParameters["Port"]/"
+    Write-Host "Setting rights on http://+:" + $OctopusParameters["Port"] +"/"
 	netsh http add urlacl url=http://+:$OctopusParameters["Port"]/ user="NT AUTHORITY\NETWORK SERVICE" | Write-Host 
 }
 Install-WebApi-Service
