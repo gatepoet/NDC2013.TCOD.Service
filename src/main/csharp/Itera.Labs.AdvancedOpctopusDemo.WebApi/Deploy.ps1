909fb6dd60c07af1df5a780a673349856c690b5e
gatepoet <kristoffer.deinoff@gmail.com>
2013-02-06 01:39:40 +0100

    fixed script(hopefully)

--- src/main/csharp/Itera.Labs.AdvancedOpctopusDemo.WebApi/Deploy.ps1
+++ src/main/csharp/Itera.Labs.AdvancedOpctopusDemo.WebApi/Deploy.ps1
@@ -9,6 +9,8 @@ function Install-WebApi-Service
     $frameworkDir = Get-FrameworkDirectory
     Set-Alias install_util (Join-Path $frameworkDir "installutil.exe")
     install_util Itera.Labs.AdvancedOpctopusDemo.WebApi.exe | Write-Host   
+
+	netsh http add urlacl url=http://+:$Env:Port/ user="NT AUTHORITY\NETWORK SERVICE"
 }
 Install-WebApi-Service
 Start-Service Itera.Labs.AdvancedOpctopusDemo.WebApi | Write-Host
