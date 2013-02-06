c408d185dc96b340e0a50f32b676a83e2a46a57f
gatepoet <kristoffer.deinoff@gmail.com>
2013-02-06 01:23:31 +0100

    fixed script(hopefully)

--- src/main/csharp/Itera.Labs.AdvancedOpctopusDemo.WebApi/Deploy.ps1
+++ src/main/csharp/Itera.Labs.AdvancedOpctopusDemo.WebApi/Deploy.ps1
@@ -3,11 +3,12 @@
     $([System.Runtime.InteropServices.RuntimeEnvironment]::GetRuntimeDirectory())
 }
 
-function Create-SQLASPNETMembership
+function Install-WebApi-Service
 {
     Write-Host "Create the ASPNETDB SQL Server database for the membership system"
     $frameworkDir = Get-FrameworkDirectory
     Set-Alias install_util (Join-Path $frameworkDir "installutil.exe")
     install_util Itera.Labs.AdvancedOpctopusDemo.WebApi.exe    
 }
-Create-SQLASPNETMembership
+Install-WebApi-Service
+Start-Service Itera.Labs.AdvancedOpctopusDemo.WebApi
